1972d11b688fc248d634517618e0fb4a927d17bd
Opilane TTHK <opilane@tthk.ee>
2021-10-29 15:36:25 +0300

    Add project files.

---
+++ Polindrom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_GG
+{
+    public class Polindrom
+    {
+        public static void PolindromFunc()
+        {
+            
+            int i = default;
+            Console.WriteLine("Enter the words");
+            string val = Console.ReadLine();
+            string[] words = val.Split(new char[] { ' ' });
+            foreach (var v in words)
+            {
+                if (v == new string(v.Reverse().ToArray()) && v.Length >= 2)
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("Words - {0} \nWords polindrom - {1} ", words.Length, i);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Kordamine_GG.Polindrom;
+namespace Kordamine_GG
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true) {
+                Console.WriteLine("\n----------------------------------\nChoose Function\n1-Polidrom \n2- \n3-");
+                int chs = int.Parse(Console.ReadLine());
+                if (chs == 1)
+                {
+                    PolindromFunc();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
